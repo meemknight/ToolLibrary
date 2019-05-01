188ee2f938437c0b9e768530375cfbd3f647ffea
vlod <vladalex420@gmail.com>
2019-05-01 15:37:38 +0300

    first working test done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
+#include "tools.h"
+#include <algorithm>
+
+
 
 int main()
 {
 
+	std::vector<int> packages = { 0, 12, 10, 14, 1, 20, 2, 12, 1, 0, 1, 0 };
+	std::partition(packages.begin(), packages.end(), arg1 < 10);
 
+	std::for_each(packages.begin(), packages.end(), [](int p) {std::cout << p << "\n"; });
+
+	std::cin.get();
+	return 0;
 }
+
--- tools.h
+++ tools.h
@@ -1,1 +1,30 @@
 #pragma once
+/////////////
+//Tools.h
+/////////////
+#include <utility>
+
+struct Arg1_t
+{
+}arg1;
+
+// T < value
+template <class T>
+struct lessThan
+{
+	lessThan(T than) :than(than) {}
+	
+	template <class U>
+	bool operator()(U &&other)
+	{
+		return (std::forward<U>(other) < than);
+	}
+
+	T than;
+};
+
+template<class T>
+lessThan<T> operator<(const Arg1_t arg1, T&& other)
+{
+	return lessThan<T>(std::forward<T>(other));
+}
