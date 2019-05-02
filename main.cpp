d6254f4c6102c6a1b84aee0e6a0b03a217f1e9d3
vlod <vladalex420@gmail.com>
2019-05-02 14:30:09 +0300

    added the basic comparison operators

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@
 #include <algorithm>
 
 
-
 int main()
-{
+{	
 
 	std::vector<int> packages = { 0, 12, 10, 14, 1, 20, 2, 12, 1, 0, 1, 0 };
-	std::partition(packages.begin(), packages.end(), arg1 < 10);
+	std::partition(packages.begin(), packages.end(), ! (arg1 <= 10));
 
 	std::for_each(packages.begin(), packages.end(), [](int p) {std::cout << p << "\n"; });
+	
+	//std::cout << (arg1 < 10)(1);
 
 	std::cin.get();
 	return 0;
--- tools.h
+++ tools.h
@@ -8,23 +8,35 @@ struct Arg1_t
 {
 }arg1;
 
-// T < value
-template <class T>
-struct lessThan
-{
-	lessThan(T than) :than(than) {}
-	
-	template <class U>
-	bool operator()(U &&other)
-	{
-		return (std::forward<U>(other) < than);
-	}
+#define	MAKE_BINARY_OPERATOR(name, op)									\
+template <class T>														\
+struct name																\
+{																		\
+	explicit name(T than) :than(than) {}								\
+	template <class U>													\
+	bool operator()(U &&other){return (std::forward<U>(other) op than);}\
+	T than;																\
+};																		\
+template<class T>														\
+name<T> operator op (const Arg1_t arg1, T&& other)						\
+{return name<T>(std::forward<T>(other));}
 
-	T than;
-};
+#define MAKE_OPOSING_BINARY_OPERATORS_ONE_WAY(name1, name2)	\
+template<class T>											\
+name1<T> operator!(const name2<T> &other)					\
+{return name1<T>(other.than);}								\
 
-template<class T>
-lessThan<T> operator<(const Arg1_t arg1, T&& other)
-{
-	return lessThan<T>(std::forward<T>(other));
-}
+#define MAKE_OPORING_BINARY_OPERATORS(name1, name2)	\
+MAKE_OPOSING_BINARY_OPERATORS_ONE_WAY(name1, name2)	\
+MAKE_OPOSING_BINARY_OPERATORS_ONE_WAY(name2, name1)	\
+
+#define MAKE_BINARY_OPERATORS_SET(name1, op1, name2, op2)\
+MAKE_BINARY_OPERATOR(name1, op1)						\
+MAKE_BINARY_OPERATOR(name2, op2)						\
+MAKE_OPORING_BINARY_OPERATORS(name1, name2);			\
+
+MAKE_BINARY_OPERATORS_SET(LessThan, <, BiggerEqualThan, >=);
+MAKE_BINARY_OPERATORS_SET(LessEqualThan, <= , BiggerThan, > );
+MAKE_BINARY_OPERATORS_SET(Equals, == , NotEquals, != );
+MAKE_BINARY_OPERATORS_SET(BinaryAnd, &, BinaryOr, |);
+
